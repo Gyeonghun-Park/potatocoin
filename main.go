69653f1f233f8de40ba2a24e8511f9b1e478088d
Gyeonghun Park <gp112795@gmail.com>
2021-11-11 17:38:48 +0900

    Refactor Code

--- main.go
+++ main.go
@@ -1,7 +1,18 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"github.com/Gyeonghun-Park/potatocoin/blockchain"
+)
 
 func main() {
-	fmt.Println("Welcome to PotatoCoin!")
+	chain := blockchain.GetBlockchain()
+	chain.AddBlock("Second Block")
+	chain.AddBlock("Third Block")
+	chain.AddBlock("Fourth Block")
+	for _, block := range chain.AllBlocks() {
+		fmt.Printf("Data: %s\n", block.Data)
+		fmt.Printf("Hash: %s\n", block.Hash)
+		fmt.Printf("Prev Hash: %s\n", block.PrevHash)
+	}
 }
