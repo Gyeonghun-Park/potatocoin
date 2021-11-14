f0ef8ed87f44707f4dc06f51d6a305648bdb66b4
Gyeonghun Park <gp112795@gmail.com>
2021-11-14 21:33:00 +0900

    Rendering Templates

--- main.go
+++ main.go
@@ -2,14 +2,23 @@ package main
 
 import (
 	"fmt"
+	"github.com/Gyeonghun-Park/potatocoin/blockchain"
+	"html/template"
 	"log"
 	"net/http"
 )
 
 const port string = ":4000"
 
+type homeData struct {
+	PageTitle string
+	Blocks    []*blockchain.Block
+}
+
 func home(rw http.ResponseWriter, r *http.Request) {
-	fmt.Fprint(rw, "Hello from home!")
+	tmpl := template.Must(template.ParseFiles("templates/home.html"))
+	data := homeData{"Home", blockchain.GetBlockchain().AllBlocks()}
+	tmpl.Execute(rw, data)
 }
 
 func main() {
