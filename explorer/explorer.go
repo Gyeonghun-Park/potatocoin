fef518226bd971359f67170c7de1a4751657a8bd
Gyeonghun Park <gp112795@gmail.com>
2021-12-01 17:46:36 +0900

    NewServeMux

--- explorer/explorer.go
+++ explorer/explorer.go
@@ -2,6 +2,7 @@ package explorer
 
 import (
 	"fmt"
+	"github.com/Gyeonghun-Park/potatocoin/utils"
 	"log"
 	"net/http"
 	"text/template"
@@ -10,7 +11,6 @@ import (
 )
 
 const (
-	port        string = ":4000"
 	templateDir string = "explorer/templates/"
 )
 
@@ -23,26 +23,27 @@ type homeData struct {
 
 func home(rw http.ResponseWriter, r *http.Request) {
 	data := homeData{"Home", blockchain.GetBlockchain().AllBlocks()}
-	templates.ExecuteTemplate(rw, "home", data)
+	utils.HandleErr(templates.ExecuteTemplate(rw, "home", data))
 }
 
 func add(rw http.ResponseWriter, r *http.Request) {
 	switch r.Method {
 	case "GET":
-		templates.ExecuteTemplate(rw, "add", nil)
+		utils.HandleErr(templates.ExecuteTemplate(rw, "add", nil))
 	case "POST":
-		r.ParseForm()
+		utils.HandleErr(r.ParseForm())
 		data := r.Form.Get("blockData")
 		blockchain.GetBlockchain().AddBlock(data)
 		http.Redirect(rw, r, "/", http.StatusPermanentRedirect)
 	}
 }
 
-func Start() {
+func Start(port int) {
+	handler := http.NewServeMux()
 	templates = template.Must(template.ParseGlob(templateDir + "pages/*.gohtml"))
 	templates = template.Must(templates.ParseGlob(templateDir + "partials/*.gohtml"))
-	http.HandleFunc("/", home)
-	http.HandleFunc("/add", add)
-	fmt.Printf("Listening on http://localhost%s\n", port)
-	log.Fatal(http.ListenAndServe(port, nil))
+	handler.HandleFunc("/", home)
+	handler.HandleFunc("/add", add)
+	fmt.Printf("Listening on http://localhost:%d\n", port)
+	log.Fatal(http.ListenAndServe(fmt.Sprintf(":%d", port), handler))
 }
